9bd8027c87610894dc4c6ffa77d6887ebfec69e3
ksader <ksader@gmail.com>
2020-03-03 09:02:58 -0800

    Refactored unit test to be data driven

--- src/test/java/org/saderfamily/ch1/UniqueCharactersTest.java
+++ src/test/java/org/saderfamily/ch1/UniqueCharactersTest.java
@@ -5,13 +5,32 @@ import org.testng.annotations.*;
 
 public class UniqueCharactersTest {
 
-    @Test
-    public void all_unique_characters() {
-        assertTrue(new UniqueCharacters().isUnique("abcde"));
+    @Test(dataProvider = "uniqueStrings")
+    public void all_unique_characters(String s, String empty) {
+        assertTrue(new UniqueCharacters().isUnique(s));
     }
 
-    @Test
-    public void duplicated_characters() {
-        assertFalse(new UniqueCharacters().isUnique("xyzzy"));
+    @Test(dataProvider = "duplicateCharacters")
+    public void duplicated_characters(String s, String empty) {
+        assertFalse(new UniqueCharacters().isUnique(s));
+    }
+
+    @DataProvider(name = "uniqueStrings")
+    public Object[][] uniqueStrings() {
+        return new Object[][]{
+                {"abcde", ""},
+                {"Janet", ""},
+                {"Keith", ""}
+        };
+    }
+
+    @DataProvider(name = "duplicateCharacters")
+    public Object[][] duplicateCharacters() {
+        return new Object[][]{
+                {"xyzzy", ""},
+                {"AAbbcc", ""},
+                {"  ", ""},
+                {"The quick brown fox jumped over the Lazy Dog", ""}
+        };
     }
 }
