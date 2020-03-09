9664c4e29275922e3434a7d80d26221bee409363
ksader <ksader@gmail.com>
2020-03-09 16:07:33 -0700

    Add test for no path existing

--- src/test/java/org/saderfamily/ch4/GraphTest.java
+++ src/test/java/org/saderfamily/ch4/GraphTest.java
@@ -4,34 +4,41 @@ import org.testng.annotations.*;
 import static org.testng.Assert.*;
 
 public class GraphTest {
-    private Graph directedWithPath;
+    private Graph directedGraph;
     private Node a;
     private Node e;
     private Node i;
     private Node m;
     private Node n;
     private Node s;
+    private Node z;
 
     @BeforeTest
     public void setup() {
-        directedWithPath = new Graph();
         a = new Node("a");
         e = new Node("e");
         i = new Node("i");
         m = new Node("m");
         n = new Node("n");
         s = new Node("s");
+        z = new Node("z");
 
-        s.children = new Node[]{a};
+        s.children = new Node[]{a, z};
         a.children = new Node[]{n, m};
         n.children = new Node[]{i};
         m.children = new Node[]{e};
 
-        directedWithPath.nodes = new Node[]{s};
+        directedGraph = new Graph();
+        directedGraph.nodes = new Node[]{s};
     }
 
     @Test
     public void pathExists() {
-        assertNotNull(directedWithPath.pathExists(s, e));
+        assertNotNull(directedGraph.pathExists(s, e));
+    }
+
+    @Test
+    public void noPathExists() {
+        assertNull(directedGraph.pathExists(a, z));
     }
 }
